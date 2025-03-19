18e5e60c40350032968782e2543f4385e3096dc9
Shuntaro Ohno <sohno@ushitora.net>
2025-03-19 16:13:18 +0900

    hash routing

--- src/main.rs
+++ src/main.rs
@@ -22,32 +22,35 @@ fn switch(route: Route) -> Html {
     }
 }
 
+#[function_component(NavBar)]
+fn navbar() -> Html {
+    html! {
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div class="container">
+                <a class="navbar-brand fw-bold" href="/my_profile_app/#/">{"My Profile App"}</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link" href="/my_profile_app/#/">{"Home"}</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/my_profile_app/#/about">{"About"}</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    }
+}
+
 #[function_component(App)] // アプリのルートコンポーネント (第2章)
 fn app() -> Html {
     html! {
-        <BrowserRouter>
-            <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-                <div class="container">
-                    <a class="navbar-brand fw-bold" href="/">{ "My Profile App "}</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav ms-auto">
-                            <li class="nav-item">
-                                <a class="nav-link text-white fw-bold" href="/">{ "Home" }</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link text-white fw-bold" href="/about">{ "About" }</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <a href="/">{ "Home" }</a>
-                    <a href="/about">{ "About" }</a>
-                </div>
-            </nav>
-            <Switch<Route> render={switch} />
-        </BrowserRouter>
+        <HashRouter>
+            <NavBar />
+            <div class="container mt-4">
+                <Switch<Route> render={switch} />
+            </div>
+        </HashRouter>
     }
 }
 
